a5eae621573a79c70edd6cce9a71584f806f238a
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-02-02 00:06:20 +0200

    lab4-4-clean

---
+++ lab4-4-clean/tests/transformer_beam_search.py
@@ -0,0 +1,33 @@
+test = {   'name': 'transformer_beam_search',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> K = 5;\n'
+                                               '>>> \n'
+                                               '>>> correct = 0;\n'
+                                               '>>> total = 0;\n'
+                                               '>>> \n'
+                                               '>>> # create beam searcher;\n'
+                                               '>>> beam_searcher = BeamSearcher(model_transformer);\n'
+                                               '>>> \n'
+                                               '>>> for batch in test_iter:\n'
+                                               '...   # Input and output\n'
+                                               '...   src, src_lengths = batch.src\n'
+                                               '...   # Predict\n'
+                                               '...   prediction, _ = beam_searcher.beam_search(src, src_lengths, K)\n'
+                                               '...   # Convert to string\n'
+                                               "...   prediction = ' '.join([TGT.vocab.itos[token] for token in prediction])\n"
+                                               "...   prediction = prediction.lstrip('<bos>').rstrip('<eos>').strip()\n"
+                                               "...   ground_truth = ' '.join([TGT.vocab.itos[token] for token in batch.tgt.view(-1)])\n"
+                                               "...   ground_truth = ground_truth.lstrip('<bos>').rstrip('<eos>').strip()\n"
+                                               '...   if ground_truth == prediction:\n'
+                                               '...     correct += 1\n'
+                                               '...     break\n'
+                                               '...   total += 1\n'
+                                               '...   \n'
+                                               '>>> correct > 0\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab4-4-clean/tests/transformer_ppl.py
@@ -0,0 +1,7 @@
+test = {   'name': 'transformer_ppl',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> test_ppl = model_transformer.evaluate_ppl(test_iter);\n>>> test_ppl >= 1\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
