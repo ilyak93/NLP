994e009740ac05d8c2563a59174abb5b9456ebed
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-02-03 00:07:21 +0200

    Delete lab4-4-clean directory

--- lab4-4-clean/tests/encoder_decoder_ppl.py
+++
@@ -1,7 +0,0 @@
-test = {   'name': 'encoder_decoder_ppl',
-    'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> test_ppl = model.evaluate_ppl(test_iter);\n>>> test_ppl >= 1\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
