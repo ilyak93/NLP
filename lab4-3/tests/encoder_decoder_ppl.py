2353370de5a96fcffb0874d9e486f200122ec2db
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-02-02 22:02:20 +0200

    lab4-3

---
+++ lab4-3/tests/encoder_decoder_ppl.py
@@ -0,0 +1,7 @@
+test = {   'name': 'encoder_decoder_ppl',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> test_ppl = model.evaluate_ppl(test_iter);\n>>> test_ppl >= 1\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
