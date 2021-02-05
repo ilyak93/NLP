d6233918bdcac601a4398bda664ff8d4c1e8e078
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-02-05 21:19:08 +0200

    lab3-4

---
+++ lab3-4/tests/pcky_fill_table.py
@@ -0,0 +1,7 @@
+test = {   'name': 'pcky_fill_table',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': ">>> table.iloc[4, 5]['S'] - 0.35 * table.iloc[4, 5]['NUM'] < 0.0001\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
